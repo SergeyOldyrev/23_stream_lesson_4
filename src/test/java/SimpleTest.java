8b82ae96affe35e20832c14c40437a7599d98af0
Sergey Oldyrev <toryn_h.e@mail.ru>
2023-11-01 07:45:46 +0300

    done

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,39 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.byValue;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SimpleTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void successfulSearchTest() {
+
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $(byText("Soft assertions")).click();
+
+        String jUnitExample ="@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}";
+        $("#user-content-3-using-junit5-extend-test-class").sibling(0).shouldHave(text(jUnitExample));
+    }
+}
